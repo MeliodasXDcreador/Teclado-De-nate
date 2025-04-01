99a64a2a8ea298fd0683e4e50921e11fed105ef6
Arthur1406 <andresmontero14062009@gmail.com>
2025-04-01 16:31:18 -0500

    copia de pagina

---
+++ script.js
@@ -0,0 +1,8 @@
+let numero = document.getElementById('num');
+let sumar = document.getElementById('mas');
+let restar = document.getElementById('menos');
+
+sumar.addEventListener("click",()=>{
+numero.textContent =numero++;
+alert('')
+});
